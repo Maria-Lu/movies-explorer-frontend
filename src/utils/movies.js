808a749c1cb633b62718099c1c59ac4c466d5fde
Maria Poskrebysheva <m.s.poskrebysheva@yandex.ru>
2021-07-25 14:54:11 +0300

    feat: add components

---
+++ src/utils/movies.js
@@ -0,0 +1,149 @@
+import image_1 from '../images/movies/pic-1.png';
+import image_2 from '../images/movies/pic-2.png';
+import image_3 from '../images/movies/pic-3.png';
+import image_4 from '../images/movies/pic-4.png';
+import image_5 from '../images/movies/pic-5.png';
+import image_6 from '../images/movies/pic-6.png';
+import image_7 from '../images/movies/pic-7.png';
+import image_8 from '../images/movies/pic-8.png';
+import image_9 from '../images/movies/pic-9.png';
+import image_10 from '../images/movies/pic-10.png';
+import image_11 from '../images/movies/pic-11.png';
+import image_12 from '../images/movies/pic-12.png';
+import image_13 from '../images/movies/pic-13.png';
+import image_14 from '../images/movies/pic-14.png';
+import image_15 from '../images/movies/pic-15.png';
+import image_16 from '../images/movies/pic-16.png';
+
+const movies = [
+  {
+    name: '33 слова о дизайне',
+    duration: '1ч 42м',
+    isLiked: true,
+    image: image_1,
+    id: 1,
+    trailer: 'https://www.youtube.com/watch?v=__2oZSM1l1Q',
+  },
+  {
+    name: 'Киноальманах «100 лет дизайна»',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_2,
+    id: 2,
+    trailer: 'https://www.youtube.com/watch?v=DpDkewhiZiI',
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_3,
+    id: 3,
+    trailer: 'https://www.youtube.com/watch?v=I7EuC1mW-8U',
+  },
+  {
+    name: 'Баския: Взрыв реальности',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_4,
+    id: 4,
+    trailer: 'https://www.youtube.com/watch?v=PUsy5RObL2U',
+  },
+  {
+    name: 'Бег это свобода',
+    duration: '1ч 42м',
+    isLiked: true,
+    image: image_5,
+    id: 5,
+    trailer: 'https://www.youtube.com/watch?v=8SwarSKGj9c',
+  },
+  {
+    name: 'Книготорговцы',
+    duration: '1ч 42м',
+    isLiked: true,
+    image: image_6,
+    id: 6,
+    trailer: 'https://www.youtube.com/watch?v=Eq2n6LJrrZY',
+  },
+  {
+    name: 'Когда я думаю о Германии ночью',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_7,
+    id: 7,
+    trailer: 'https://youtu.be/KTTT08_xD-0',
+  },
+  {
+    name: 'Gimme Danger: История Игги и The Stooge',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_8,
+    id: 8,
+    trailer: 'https://www.youtube.com/watch?v=Eonj7iDlfHw',
+  },
+  {
+    name: 'Дженис: Маленькая девочка грустит',
+    duration: '1ч 42м',
+    isLiked: true,
+    image: image_9,
+    id: 9,
+    trailer: 'https://www.youtube.com/watch?v=Hefit0jYmfw&t=1s',
+  },
+  {
+    name: 'Соберись перед прыжком',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_10,
+    id: 10,
+    trailer: 'https://www.youtube.com/watch?v=n5Vm_Awe3bw',
+  },
+  {
+    name: 'Пи Джей Харви: A dog called money',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_11,
+    id: 11,
+    trailer: 'https://www.youtube.com/watch?v=Od987fEKznw',
+  },
+  {
+    name: 'По волнам: Искусство звука в кино',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_12,
+    id: 12,
+    trailer: 'https://www.youtube.com/watch?v=WsIE4x3xU0Y',
+  },
+  {
+    name: 'Рудбой',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_13,
+    id: 13,
+    trailer: 'https://www.youtube.com/watch?v=wWFRdG4y0eg',
+  },
+  {
+    name: 'Скейт-кухня',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_14,
+    id: 14,
+    trailer: 'https://www.youtube.com/watch?v=iT1izrIxoos',
+  },
+  {
+    name: 'Война искусств',
+    duration: '1ч 42м',
+    isLiked: false,
+    image: image_15,
+    id: 15,
+    trailer: 'https://www.youtube.com/watch?v=NOmiotihS_0',
+  },
+  {
+    name: 'Зона',
+    duration: '1ч 42м',
+    isLiked: true,
+    image: image_16,
+    id: 16,
+    trailer: 'https://www.youtube.com/watch?v=WOra77ZElWI',
+  },
+];
+
+export default movies;
